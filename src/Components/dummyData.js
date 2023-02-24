1ba0c504460c51ac2b6e4f8538cba799357da090
Rayhan <maouslayer@gmail.com>
2023-02-24 09:35:02 +0000

    Add dummy data of expected doctors details api call

---
+++ src/Components/dummyData.js
@@ -0,0 +1,72 @@
+const dummyData = [
+  {
+    id: 1,
+    name: "Dr. John Doe",
+    specialization: "Cardiologist",
+    photo: "https://example.com/doctors/1.jpg",
+    contact: "+1-555-555-5555",
+    availableDates: [
+      {
+        date: "2022-03-01",
+        availableTimes: [
+          "10:00 AM",
+          "11:00 AM",
+          "12:00 PM"
+        ]
+      },
+      {
+        date: "2022-03-02",
+        availableTimes: [
+          "11:00 AM",
+          "1:00 PM",
+          "3:00 PM"
+        ]
+      },
+      {
+        date: "2022-03-03",
+        availableTimes: [
+          "9:00 AM",
+          "10:00 AM",
+          "11:00 AM"
+        ]
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: "Dr. Jane Smith",
+    specialization: "Dermatologist",
+    photo: "https://example.com/doctors/2.jpg",
+    contact: "+1-555-555-5555",
+    availableDates: [
+      {
+        date: "2022-03-01",
+        availableTimes: [
+          "9:00 AM",
+          "11:00 AM",
+          "2:00 PM"
+        ]
+      },
+      {
+        date: "2022-03-02",
+        availableTimes: [
+          "10:00 AM",
+          "12:00 PM",
+          "4:00 PM"
+        ]
+      },
+      {
+        date: "2022-03-03",
+        availableTimes: [
+          "11:00 AM",
+          "2:00 PM",
+          "3:00 PM"
+        ]
+      }
+    ]
+  }
+];
+
+export default dummyData;
+// import dummyData from './dummyData';
+
