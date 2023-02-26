511c387cbfabd2c78e7a16ac53da2d97569eb40f
devMunyi <samunyi90@gmail.com>
2023-02-26 21:36:37 +0300

    add side navigation bar

--- src/pages/Reserve.js
+++ src/pages/Reserve.js
@@ -3,9 +3,11 @@ import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
 import { DatePicker } from 'antd';
 import { toast } from 'react-toastify';
+import { SyncOutlined } from '@ant-design/icons';
 
 function Reserve() {
   const [loading, setLoading] = useState(false);
+  const [fetchingData, setFetchingData] = useState(false);
   const [date, setDate] = useState('');
   const [doctor, setDoctor] = useState(null);
 
@@ -15,10 +17,15 @@ function Reserve() {
   useEffect(() => {
     const fetchDoctor = async () => {
       try {
-        const response = await axios.get(`http://localhost:3000/api/users/${userId}/doctors/${doctorId}`);
+        setFetchingData(true);
+        const response = await axios.get(
+          `http://localhost:3000/api/users/${userId}/doctors/${doctorId}`,
+        );
         setDoctor(response.data);
       } catch (error) {
         console.log(error);
+      } finally {
+        setFetchingData(false);
       }
     };
 
@@ -65,55 +72,68 @@ function Reserve() {
   return (
     <div id="reserve-page" className="reservePage">
       <div className="container-fluid reservePage">
-        <img
-          src={`${doctor?.picture}`}
-          alt="Doctor"
-          style={{ width: '100%', height: '100vh' }}
-        />
-        <div className="centered">
-          <h1 className="doc-name pb-2">{`Dr ${doctor?.name} - ${doctor?.specialization}`}</h1>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus
-            provident et expedita perferendis dolore perspiciatis odio.
-            Dignissimos, in deleniti pariatur alias odit explicabo vitae libero
-            provident est placeat consectetur hic?
-          </p>
+        {fetchingData ? (
+          <div className="d-flex justify-content-center align-items-center" style={{ width: '100%', height: '100vh', backgroundColor: '#589d1a' }}>
+            <SyncOutlined spin style={{ fontSize: '32px' }} />
+          </div>
 
-          <div className="d-flex gap-3 justify-content-center">
-            <DatePicker
-              showTime
-              onChange={handleDateChange}
-              className="date-picker"
+        ) : (
+          <>
+            <img
+              src={`${doctor?.picture}`}
+              alt="Doctor"
+              style={{ width: '100%', height: '100vh' }}
             />
-          </div>
+            <div className="centered">
+              <h1 className="doc-name pb-2">{`Dr ${doctor?.name} - ${doctor?.specialization}`}</h1>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus
+                provident et expedita perferendis dolore perspiciatis odio.
+                Dignissimos, in deleniti pariatur alias odit explicabo vitae
+                libero provident est placeat consectetur hic?
+              </p>
 
-          <div className="d-flex gap-3 justify-content-center mt-4">
-            <Link
-              to={`/doctors/${doctorId}`}
-              className="btn btn-outline-light pb-0"
-            >
-              Go Back
-            </Link>
-            {loading ? (
-              <button className="btn btn-outline-light" type="button" disabled>
-                <span
-                  className="spinner-border spinner-border-sm"
-                  role="status"
-                  aria-hidden="true"
+              <div className="d-flex gap-3 justify-content-center">
+                <DatePicker
+                  showTime
+                  onChange={handleDateChange}
+                  className="date-picker"
                 />
-                <span className="visually-hidden">Loading...</span>
-              </button>
-            ) : (
-              <button
-                type="submit"
-                onClick={handleBook}
-                className="btn btn-outline-light"
-              >
-                Book Now
-              </button>
-            )}
-          </div>
-        </div>
+              </div>
+
+              <div className="d-flex gap-3 justify-content-center mt-4">
+                <Link
+                  to={`/doctors/${doctorId}`}
+                  className="btn btn-outline-light pb-0"
+                >
+                  Go Back
+                </Link>
+                {loading ? (
+                  <button
+                    className="btn btn-outline-light"
+                    type="button"
+                    disabled
+                  >
+                    <span
+                      className="spinner-border spinner-border-sm"
+                      role="status"
+                      aria-hidden="true"
+                    />
+                    <span className="visually-hidden">Loading...</span>
+                  </button>
+                ) : (
+                  <button
+                    type="submit"
+                    onClick={handleBook}
+                    className="btn btn-outline-light"
+                  >
+                    Book Now
+                  </button>
+                )}
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
