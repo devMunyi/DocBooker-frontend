65bea1e724e80a0a75e04eeb31392aa4a248aa53
salimabdulai <maouslayer@gmail.com>
2023-03-01 08:04:26 +0000

    Add logic to prevent navigation until user logs in

--- src/components/DoctorDetails.js
+++ src/components/DoctorDetails.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+<<<<<<< HEAD:src/components/DoctorDetails.js
 import 'react-datepicker/dist/react-datepicker.css';
 import './css/doctor_details.css';
 
@@ -8,6 +9,29 @@ function DoctorDetails({ doctor }) {
     <div className="doctor-details">
       <div className="doctor-details__header">
         <h2>{doctor.name}</h2>
+=======
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import { doctorAdded } from '../Redux/reducers/selectedDoctor';
+
+function DoctorCard({ doctor }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const detailsUrl = '/doctors/1';
+
+  const doctorSelected = async () => {
+    dispatch(doctorAdded(doctor));
+    navigate(detailsUrl);
+  };
+
+  return (
+    <div className="doctor-details">
+      <div className="doctor-details__header">
+        <button type="button" onClick={doctorSelected} className="btn btn-success btn-sm">
+          <h2>{doctor.name}</h2>
+        </button>
+>>>>>>> e56034d (Add logic to prevent navigation until user logs in):src/Components/DoctorCard.js
         <h3>{doctor.specialization}</h3>
       </div>
       <div className="doctor-details__body">
