daf9c19b1a689b7f6ad6208474d0ce9a257fddae
salimabdulai <maouslayer@gmail.com>
2023-03-01 09:13:05 +0000

    Fix linters

--- src/pages/SingleDoctor.js
+++ src/pages/SingleDoctor.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-
 import DoctorDetails from '../components/DoctorDetails';
 
 function SingleDoctor() {
