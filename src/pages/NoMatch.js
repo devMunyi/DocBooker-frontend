a270d3013b13762af323629f4f93e81d73e89a25
devMunyi <samunyi90@gmail.com>
2023-03-05 15:42:54 +0300

    Remove Sidebar in NoMatch Page

--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import NavBar from '../components/NavBar';
 
 function NoMatch() {
   return (
     <div className="page">
-      <NavBar />
       <main className="content">
         <div className="text-center fs">
           404 NOT FOUND!
