5b7999286036f12d2ac2374e817f358935d6f04a
salimabdulai <maouslayer@gmail.com>
2023-03-06 10:14:06 +0000

    Add style to buttons

--- src/components/DoctorDetails.js
+++ src/components/DoctorDetails.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import 'react-datepicker/dist/react-datepicker.css';
 import './css/doctor_details.css';
 
