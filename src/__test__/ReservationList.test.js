0f8cadfa6da0a5efe9f0450d9c6ae5df6b5ce2fd
Tetteh Julius <juliustetteh@gmail.com>
2023-03-07 18:27:20 +0000

    Add test for Reservation List Component

---
+++ src/__test__/ReservationList.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { MemoryRouter as Router } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import store from '../Redux/store';
+import ReservationList from '../pages/ReservationList';
+
+describe('ReservationList component', () => {
+  
+it('renders without crashing', () => {
+    localStorage.setItem('user', JSON.stringify({ id: 123 }));
+    const { getAllByText } = render(
+      <Provider store={store}>
+        <Router>
+          <ReservationList />
+        </Router>
+      </Provider>,
+    );
+    const headers = getAllByText('My Reservations');
+    expect(headers[0]).toBeInTheDocument();
+  });  
+});
